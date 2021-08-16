2dbe1d547d1f540b46882d46c35b2a0a1feee211
VedantS20 <vedantsasane2005@gmail.com>
2021-08-16 23:45:20 +0530

    build

---
+++ build/precache-manifest.31e278fa86b9ea1d2a807e4ca913a2b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd8e4bc6fe1b04cbc575c30986ad9bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f52c8b47003a1bfdd2e1",
+    "url": "/static/css/main.890f977b.chunk.css"
+  },
+  {
+    "revision": "4c1f9e42fa48abfdcdfc",
+    "url": "/static/js/2.9776d67f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9776d67f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52c8b47003a1bfdd2e1",
+    "url": "/static/js/main.2a8c0a74.chunk.js"
+  },
+  {
+    "revision": "72017dee5443dfc32ffd",
+    "url": "/static/js/runtime-main.dc49cf19.js"
+  }
+]);
